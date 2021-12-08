b0569f8fe0ac64032c371705922aa6d16ddd8b2e
tomatogu3 <55967243+tomatogu3@users.noreply.github.com>
2021-12-08 14:41:16 +0900

    ShopDetails遷移処理追加

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shopdetails/', views.ShopDetails, name='ShopDetail')
+    path('shopdetails/', views.ShopDetails, name='ShopDetails')
 ]
