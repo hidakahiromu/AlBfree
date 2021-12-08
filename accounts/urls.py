ce102768f40541987847b15f3cd0110d73221537
tomatogu3 <55967243+tomatogu3@users.noreply.github.com>
2021-12-08 09:56:09 +0900

    urls.pyに店舗詳細の遷移処理追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
- 
+
 urlpatterns = [
     path('templates/', views.index_template, name='index_template'),
-    path('' , views.TopPage , name='TopPage'),
+    path('', views.TopPage, name='TopPage'),
+    path('shopdetails/', views.ShopDetails, name='ShopDetail')
+
 ]
