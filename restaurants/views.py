b80032cdc691af3f5633eff99fad345b7a2b0375
日高 広夢 <chiro3330@gmail.com>
2021-12-08 14:35:27 +0900

    urlの変更（一覧画面対応）

--- restaurants/views.py
+++ restaurants/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def ShopDetails(request):
     return render(request, 'ShopDetails.html')
+
+def RestaurantsList(request):
+    return render(request , 'restaurantsList.html')
