bb2384a05e6e1194f5e4bac6f919f82917d33353
日高 広夢 <chiro3330@gmail.com>
2021-12-09 14:54:18 +0900

    htmlにデータを表示

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import store_information
 
 # Create your views here.
 def ShopDetails(request):
     return render(request, 'ShopDetails.html')
 
 def RestaurantsList(request):
-    return render(request , 'restaurantsList.html')
+    db = {
+        'test_list' : store_information.objects.all()
+    }
+    return render(request , 'restaurantsList.html' , db)
