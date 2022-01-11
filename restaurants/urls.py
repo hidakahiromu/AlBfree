5576bc088d439aa00b78e35dae628e0a613a0d11
日高 広夢 <chiro3330@gmail.com>
2022-01-11 13:03:32 +0900

    各フォームの作成及びデータベースの名前変更
    restaurantを消したり、storeを消したりして統一感を持たせました

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -2,8 +2,12 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('storedetails/', views.StoreDetails, name='StoreDetail'),
     path('list/', views.RestaurantsList, name='RestaurantsList'),
-    path('modelForm/' , views.Form , name="Form"),
+    path('restaurantimageform/' , views.RestaurantImageForm , name="RestaurantImageForm"),
+    path('restaurantsform/' , views.RestaurantsForm , name='RestaurantsForm'),
+    path('restaurantmenuform/' , views.RestaurantMenuForm , name='RestaurantMenuForm'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect
 from django.conf import settings
-from .forms import store_imagesForm
-from .models import store_information , store_images , store_menu
+from .forms import restaurantInformationForm , restaurantImagesForm , restaurantMenusForm
+from .models import restaurant_information , images , menus
 
 
 # Create your views here.
@@ -13,23 +13,52 @@ def StoreDetails(request):
 #飲食店一覧画面
 def RestaurantsList(request):
     db = {
-        'restaurants_db' : store_information.objects.all(),
+        'restaurants_db' : information.objects.all(),
         #'user_db' : store_information.objects.get(contributor = 0),
     }
     return render(request , 'restaurantsList.html' , db)
 
-
-#飲食店の画像(飲食店のトップ画像など)投稿フォーム　※後で関数名は変えるかも
-def Form(request):
+#飲食店の情報投稿フォーム
+def RestaurantsForm(request):
     if request.method == 'POST':
-        form = store_imagesForm(request.POST , request.FILES)
+        form = restaurantInformationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('Form')
+            return redirect('RestaurantsForm')
     else:
-        form = store_imagesForm()
+        form = restaurantInformationForm()
     
 
-    return render(request, 'modelForm.html', {
+    return render(request, 'restaurantsInformationForm.html', {
+        'form': form
+    })
+
+#飲食店のメニュー投稿フォーム
+def RestaurantMenuForm(request):
+    if request.method == 'POST':
+        form = restaurantMenusForm(request.POST , request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('RestaurantMenuForm')
+    else:
+        form = restaurantMenusForm()
+    
+
+    return render(request, 'restaurantMenusForm.html', {
+        'form': form
+    })
+
+#飲食店の画像(飲食店のトップ画像など)投稿フォーム　※後で関数名は変えるかも
+def RestaurantImageForm(request):
+    if request.method == 'POST':
+        form = restaurantImagesForm(request.POST , request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('RestaurantImagesForm')
+    else:
+        form = restaurantImagesForm()
+    
+
+    return render(request, 'restaurantImageForm.html', {
         'form': form
     })
