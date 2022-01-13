145a80bf77a3e18740692ffaeb463dcb67d115f2
nissy <>
2022-01-13 10:01:09 +0900

    リンク紐付け(途中)

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
  #http://127.0.0.1:8000/XXX/
     path('', views.TopPage, name='TopPage'),    #トップページ
-    path('storedetails/', views.StoreDetails, name='ShopDetails'),  #店舗詳細
+    #path('storedetails/', views.StoreDetails, name='StoreDetails'),  #店舗詳細
     path('registration/' , views.registration , name='registration'),   #アカウント新規作成
     path('terms/' , views.terms , name='terms'), #利用規約
     path('forstores/' , views.forstores , name='forstores'), #店舗向け案内
